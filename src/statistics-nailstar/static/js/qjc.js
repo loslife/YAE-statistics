ea96c9091f347950e8db84d3e8a1245745ba0fd8
小龙龙 <1240429156@qq.com>
2015-09-10 18:51:46 +0800

    no message

--- src/statistics-nailstar/static/js/qjc.js
+++ src/statistics-nailstar/static/js/qjc.js
@@ -8,31 +8,6 @@ app.controller('dakaQjcCtrl', ['$rootScope', '$scope', '$http', function ($rootS
         recentPhoto: 10,
     };
 
-    $scope.qjcPicCount = function (recentPhoto){
-        $http.get("/svc/dakatongji/qjcPicCount?num=" + recentPhoto).success(function(data) {
-
-            var details = data.result.details;
-
-            $scope.pic_result = _.map(details, function(element){
-                return [element.no, element.count];
-            });
-            console.log($scope.pic_result);
-
-        }).error(function(data, status) {
-
-            console.log("qjcPicCount in error");
-
-        });
-    };
-
-    $scope.qjcPicCount($scope.course.recentPhoto);
-
-    $scope.$watch('course',function(newVal,oldVal){
-        if(newVal !== oldVal && newVal.recentPhoto !== oldVal.recentPhoto) {
-            $scope.qjcPicCount($scope.course.recentPhoto);
-        }
-    },true);
-
     $scope.qjcVoteCount = function (recentVote){
         $http.get("/svc/dakatongji/qjcVoteCount?num=" + recentVote).success(function(data) {
 
@@ -58,8 +33,52 @@ app.controller('dakaQjcCtrl', ['$rootScope', '$scope', '$http', function ($rootS
 
     $scope.qjcVoteCount($scope.course.recentVote);
 
-    //$scope.test = function(){
-    //    alert(455);
-    //}
+    $scope.qjcPicCount = function (recentPhoto){
+        $http.get("/svc/dakatongji/qjcPicCount?num=" + recentPhoto).success(function(data) {
+
+            var details = data.result.details;
+
+            $scope.pic_result = _.map(details, function(element){
+                return [element.no, element.count];
+            });
+            console.log($scope.pic_result);
+
+        }).error(function(data, status) {
+
+            console.log("qjcPicCount in error");
+
+        });
+    };
+
+    $scope.qjcPicCount($scope.course.recentPhoto);
+
+    $scope.$watch('course',function(newVal,oldVal){
+        if(newVal !== oldVal && newVal.recentPhoto !== oldVal.recentPhoto) {
+            $scope.qjcPicCount($scope.course.recentPhoto);
+        }
+    },true);
+
+   /* $scope.getNewestActivity = function(recentVote){
+        $http.get("/svc/dakatongji/qjcVoteCount?num=" + recentVote).success(function(data) {
+
+            var details = data.result.details;
+            var length = details.length;
+            if(recentVote !== length){
+                for(i=0; i<recentVote; i++){
+
+                }
+            }
+
+            $scope.vote_result = _.map(details, function(element){
+                return [element.no, element.count];
+            });
+            console.log($scope.vote_result);
+
+        }).error(function(data, status) {
+
+            console.log("qjcVoteCount in error");
+
+        });
+    }*/
 
 }]);
