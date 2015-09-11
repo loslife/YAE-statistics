49e72a1661313ebb665776d921a75ac8df476028
keller35 <524495217@qq.com>
2015-09-11 16:57:12 +0800

    播放统计

--- src/framework/static/js/chain.js
+++ src/framework/static/js/chain.js
@@ -452,6 +452,7 @@ var app = angular.module('app', [
                 'js/jquery/charts/flot/jquery.flot.resize.js',
                 'js/jquery/charts/flot/jquery.flot.tooltip.min.js',
                 'js/jquery/charts/flot/jquery.flot.spline.js',
+                'js/jquery/charts/flot/jquery.flot.time.min.js',
                 'js/jquery/charts/flot/jquery.flot.orderBars.js',
                 'js/jquery/charts/flot/jquery.flot.pie.min.js'],
             slimScroll:     ['js/jquery/slimscroll/jquery.slimscroll.min.js'],
