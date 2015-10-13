16bc603577ab3188eb7e753b92a38a1b30d74822
keller35 <524495217@qq.com>
2015-10-13 11:33:51 +0800

    增加会员信息统计接口

---
+++ docs/member-api.md
@@ -0,0 +1,62 @@
+## 经验值排行榜
+
+url: /svc/dakatongji/member/expRanking
+    
+query:
+    
+    num=20
+    
+response:
+
+	[
+        {
+          "nickname": "勿忘我",
+          "exp": 888
+        },
+        {
+          "nickname": "手机用户2891",
+          "exp": 0
+        },
+        {
+          "nickname": "手机用户5055",
+          "exp": 0
+        }
+    ]
+    
+## 咖币排行榜
+
+url: /svc/dakatongji/member/coinRanking
+    
+query:
+    
+    num=20
+    
+response:
+
+	[
+        {
+          "nickname": "勿忘我",
+          "coin": 2000
+        },
+        {
+          "nickname": "手机用户2891",
+          "coin": 0
+        },
+        {
+          "nickname": "手机用户5055",
+          "coin": 0
+        }
+    ]
+    
+## 咖币排行榜
+
+url: /svc/dakatongji/member/coinAnalyse
+    
+response:
+
+	{
+        "总经验值": 888,
+        "注册用户人均经验值": 0.17738713543747503,
+        "总咖币": 2000,
+        "注册用户人均咖币": 0.3995205753096284
+    }
---
+++ src/statistics-nailstar/service/member.js
@@ -0,0 +1,87 @@
+var _ = require("underscore");
+var async = require("async");
+var dbHelper = require(FRAMEWORKPATH + "/utils/dbHelper");
+
+exports.expRanking = expRanking;
+exports.coinRanking = coinRanking;
+exports.coinAnalyse = coinAnalyse;
+
+//经验值排行榜
+function expRanking(req, res, next){
+
+    var num = parseInt(req.query.num) || 20;
+
+    var sql = "select nickname,exp from accounts order by exp desc limit 0,:num";
+    dbHelper.execSql(sql, {num: num}, function (err, result) {
+        if (err) {
+            return next(err);
+        }
+        doResponse(req, res, result);
+    });
+}
+
+//咖币排行榜
+function coinRanking(req, res, next){
+
+    var num = parseInt(req.query.num) || 20;
+
+    var sql = "select nickname,coin from accounts order by coin desc limit 0,:num";
+    dbHelper.execSql(sql, {num: num}, function (err, result) {
+        if (err) {
+            return next(err);
+        }
+        doResponse(req, res, result);
+    });
+}
+
+//注册用户经验、咖币的总数和平均数
+function coinAnalyse(req, res, next){
+
+    var final_result = {
+        exps: 0,
+        coins: 0,
+        avgexp: 0,
+        avgcoin: 0
+    };
+    async.series([_queryExpAndCoinCount, _queryAverageExpAndCoin], function(err){
+        if(err){
+            return next(err);
+        }
+        var result = {
+            "总经验值": final_result.exps,
+            "注册用户人均经验值": final_result.avgexp,
+            "总咖币": final_result.coins,
+            "注册用户人均咖币": final_result.avgcoin
+        };
+        doResponse(req, res, result);
+    });
+
+    function _queryExpAndCoinCount(nextStep){
+
+        var sql = "select sum(exp) 'exps',sum(coin) 'coins' from accounts";
+        dbHelper.execSql(sql, {}, function (err, result) {
+            if (err) {
+                return nextStep(err);
+            }
+            if(result && result[0]){
+                final_result.exps = result[0].exps;
+                final_result.coins = result[0].coins;
+            }
+            nextStep();
+        });
+    }
+
+    function _queryAverageExpAndCoin(nextStep){
+        var sql = "select count(1) 'count' from accounts";
+        dbHelper.execSql(sql, {}, function (err, result) {
+            if (err) {
+                return nextStep(err);
+            }
+            if(result && result[0]){
+                final_result.avgexp = (final_result.exps / result[0].count).toFixed(2);
+                final_result.avgcoin = (final_result.coins / result[0].count).toFixed(2);
+            }
+            nextStep();
+        });
+    }
+}
