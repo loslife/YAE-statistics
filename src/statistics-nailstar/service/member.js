8172d611e1366094791831e32d63281affa6a270
keller35 <524495217@qq.com>
2015-10-13 14:57:32 +0800

    注册用户每日获得经验、咖币的总数和平均数接口

--- src/statistics-nailstar/service/member.js
+++ src/statistics-nailstar/service/member.js
@@ -5,6 +5,7 @@ var dbHelper = require(FRAMEWORKPATH + "/utils/dbHelper");
 exports.expRanking = expRanking;
 exports.coinRanking = coinRanking;
 exports.expAndCoinAnalyse = expAndCoinAnalyse;
+exports.expAndCoinCount = expAndCoinCount;
 
 //经验值排行榜
 function expRanking(req, res, next){
@@ -80,6 +81,54 @@ function expAndCoinAnalyse(req, res, next){
             if(result && result[0]){
                 final_result.avgexp = (final_result.exps / result[0].count).toFixed(2);
                 final_result.avgcoin = (final_result.coins / result[0].count).toFixed(2);
+            }
+            nextStep();
+        });
+    }
+}
+
+//注册用户每日获得经验、咖币的总数和平均数
+function expAndCoinCount(req, res, next){
+
+    var num = parseInt(req.query.num) || 20;
+
+    var accountsCount = 0;
+    var final_result = [];
+    async.series([_queryAccountsCount, _queryExpAndCoinCount], function(err){
+        if(err){
+            return next(err);
+        }
+        doResponse(req, res, final_result);
+    });
+
+    function _queryAccountsCount(nextStep){
+        var sql = "select count(1) 'count' from accounts";
+        dbHelper.execSql(sql, {}, function (err, result) {
+            if (err) {
+                return nextStep(err);
+            }
+            if(result && result[0]){
+                accountsCount = result[0].count;
+            }
+            nextStep();
+        });
+    }
+
+    function _queryExpAndCoinCount(nextStep){
+
+        var sql = "select from_unixtime(action_date/1000, '%Y%m%d') as 'day'," +
+            "sum(get_exp) 'exps',sum(get_coin) 'coins'," +
+            "sum(get_exp)/" + accountsCount + " as 'avgexp', sum(get_coin)/" + accountsCount + " as 'avgcoin'" +
+            "from actions_history " +
+            "where FROM_UNIXTIME( action_date/1000, '%Y%m%d' ) " +
+            "between date_format(date_add(now(), interval -" + num + " day), '%Y%m%d') and date_format(now(), '%Y%m%d') " +
+            "group by day order by day desc";
+        dbHelper.execSql(sql, {}, function (err, result) {
+            if (err) {
+                return nextStep(err);
+            }
+            if(result){
+                final_result = result;
             }
             nextStep();
         });
