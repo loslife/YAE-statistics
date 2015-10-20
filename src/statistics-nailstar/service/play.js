ff52d0e1b32d0dade079a922d40876af48457e98
keller35 <524495217@qq.com>
2015-10-20 10:04:55 +0800

    增加通用接口文件

--- src/statistics-nailstar/service/play.js
+++ src/statistics-nailstar/service/play.js
@@ -2,33 +2,10 @@ var _ = require("underscore");
 var async = require("async");
 var dbHelper = require(FRAMEWORKPATH + "/utils/dbHelper");
 
-exports.getCategories = getCategories;
-exports.getTopics = getTopics;
 exports.getplayAll = getplayAll;
 exports.getplayByCate = getplayByCate;
 exports.getplayByTopic = getplayByTopic;
 
-//获取视频分类列表接口
-function getCategories(req, res, next) {
-    var sql = "select id,name from topic_categories order by serial_number";
-    dbHelper.execSql(sql, {}, function (err, data) {
-        if (err) {
-            return next(err);
-        }
-        doResponse(req, res, {cates: data});
-    });
-}
-
-//获取视频主题列表接口
-function getTopics(req, res, next){
-    var sql = "select id,title 'name' from topics order by create_date desc";
-    dbHelper.execSql(sql, {}, function (err, data) {
-        if (err) {
-            return next(err);
-        }
-        doResponse(req, res, {topics: data});
-    });
-}
 //美甲大咖总播放数
 function getplayAll(req, res, next){
 
