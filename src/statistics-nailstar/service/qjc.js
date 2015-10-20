c34b3a19e57d658a2258cec0bfd2b2617ee15420
keller35 <524495217@qq.com>
2015-10-20 14:51:52 +0800

    求教程视频相关接口

--- src/statistics-nailstar/service/qjc.js
+++ src/statistics-nailstar/service/qjc.js
@@ -5,6 +5,8 @@ var dbHelper = require(FRAMEWORKPATH + "/utils/dbHelper");
 exports.getNewestActivity = getNewestActivity;
 exports.qjcPicCount = qjcPicCount;
 exports.qjcVoteCount = qjcVoteCount;
+exports.qjcPlayCount = qjcPlayCount;
+exports.qjcCommentsCount = qjcCommentsCount;
 
 //获取最新活动期数
 function getNewestActivity(req, res, next){
@@ -18,8 +20,8 @@ function getNewestActivity(req, res, next){
     });
 }
 
-//求教程每期上传图片数量统计接口
-function qjcPicCount(req, res, next) {
+//求教程每期投票总数统计接口
+function qjcVoteCount(req, res, next) {
 
     var num = parseInt(req.query["num"]) || 10;
 
@@ -36,8 +38,8 @@ function qjcPicCount(req, res, next) {
     });
 }
 
-//求教程每期投票总数统计接口
-function qjcVoteCount(req, res, next) {
+//求教程每期上传图片数量统计接口
+function qjcPicCount(req, res, next) {
 
     var num = parseInt(req.query["num"]) || 10;
 
@@ -45,6 +47,40 @@ function qjcVoteCount(req, res, next) {
         "from candidates a join activities b ON a.activity_id = b.id " +
         "group by b.id order by b.id desc limit 0,:num";
 
+    dbHelper.execSql(sql, {num: num}, function (err, result) {
+        if (err) {
+            return next(err);
+        }
+        var data = {details: result};
+        doResponse(req, res, data);
+    });
+}
+
+//求教程视频播放数
+function qjcPlayCount(req, res, next){
+
+    var num = parseInt(req.query["num"]) || 10;
+
+    var sql = "select a.id 'no',IFNULL(c.play_times,0) 'count' " +
+        "from activities a left join topics_has_videos b on a.topic_id = b.topic_id" +
+        " left join videos c on b.video_id = c.id order by no desc limit 0,:num";
+    dbHelper.execSql(sql, {num: num}, function (err, result) {
+        if (err) {
+            return next(err);
+        }
+        var data = {details: result};
+        doResponse(req, res, data);
+    });
+}
+
+//求教程视频评论数
+function qjcCommentsCount(req, res, next){
+
+    var num = parseInt(req.query["num"]) || 10;
+
+    var sql = "select a.id 'no',count(a.id) 'count' " +
+        "from activities a left join comments b on a.topic_id = b.topic_id " +
+        "group by no desc limit 0,:num";
     dbHelper.execSql(sql, {num: num}, function (err, result) {
         if (err) {
             return next(err);
