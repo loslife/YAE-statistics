52a77452c47e2784c8dc41152a079e3e2fee7022
小龙龙 <1240429156@qq.com>
2015-10-21 18:58:51 +0800

    no message

--- src/statistics-nailstar/static/js/slider/sliderDetails.js
+++ src/statistics-nailstar/static/js/slider/sliderDetails.js
@@ -1,11 +1,60 @@
-app.controller('dakasliderdetails', ['$rootScope', '$scope', '$http', function ($rootScope, $scope, $http) {
+app.controller('GridDemoCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.filterOptions = {
+        filterText: "",
+        useExternalFilter: true
+    };
+    $scope.totalServerItems = 0;
+    $scope.pagingOptions = {
+        pageSizes: [250, 500, 1000],
+        pageSize: 250,
+        currentPage: 1
+    };
+    $scope.setPagingData = function(data, page, pageSize){
+        var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
+        $scope.myData = pagedData;
+        $scope.totalServerItems = data.length;
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
+    };
+    $scope.getPagedDataAsync = function (pageSize, page, searchText) {
+        setTimeout(function () {
+            var data;
+            if (searchText) {
+                var ft = searchText.toLowerCase();
+                $http.get('/svc/dakatongji/posters').success(function (largeLoad) {
+                    data = largeLoad.filter(function(item) {
+                        return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
+                    });
+                    $scope.setPagingData(data,page,pageSize);
+                });
+            } else {
+                $http.get('/svc/dakatongji/posters').success(function (largeLoad) {
+                    $scope.setPagingData(largeLoad,page,pageSize);
+                });
+            }
+        }, 100);
+    };
 
-    $rootScope.promise = $http.get("/svc/dakatongji/posters", {}).success(function(data) {
+    $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
 
-        $scope.infos = data.result;
+    $scope.$watch('pagingOptions', function (newVal, oldVal) {
+        if (newVal !== oldVal && newVal.currentPage !== oldVal.currentPage) {
+            $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+        }
+    }, true);
+    $scope.$watch('filterOptions', function (newVal, oldVal) {
+        if (newVal !== oldVal) {
+            $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+        }
+    }, true);
 
-    }).error(function(data, status) {
-
-    });
-
+    $scope.gridOptions = {
+        data: 'myData',
+        enablePaging: true,
+        showFooter: true,
+        totalServerItems: 'totalServerItems',
+        pagingOptions: $scope.pagingOptions,
+        filterOptions: $scope.filterOptions
+    };
 }]);
