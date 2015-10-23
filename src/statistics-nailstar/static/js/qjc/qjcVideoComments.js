6b60090572069e1f37989af2e230229127b77f56
小龙龙 <1240429156@qq.com>
2015-10-23 11:05:15 +0800

    修改求教程页命名规范

--- src/statistics-nailstar/static/js/qjc/qjcVideoComments.js
+++ src/statistics-nailstar/static/js/qjc/qjcVideoComments.js
@@ -5,25 +5,25 @@ app.controller('dakaqjcvideocomments', ['$rootScope', '$scope', '$http', 'utilsS
     })();
 
     function initVote(){
-        $scope.vote_result_x = [0,0,0,0,0];
-        $scope.vote_result_y = [0,0,0,0,0];
+        $scope.comments_result_x = [0,0,0,0,0];
+        $scope.comments_result_y = [0,0,0,0,0];
 
         //默认参数
-        $scope.vote = {
-            recentVote: 20,
+        $scope.CommentsParams = {
+            num: 20,
         };
 
         //数据缓存
-        var voteDataCacheX = {};
-        var voteDataCacheY = {};
+        var commentsDataCacheX = {};
+        var commentsDataCacheY = {};
 
         //获取投票参数
         function getVote(num) {
 
-            if(voteDataCacheX[num] && voteDataCacheY[num]){
+            if(commentsDataCacheX[num] && commentsDataCacheY[num]){
 
-                $scope.vote_result_x = voteDataCacheX[num];
-                $scope.vote_result_y = voteDataCacheY[num];
+                $scope.comments_result_x = commentsDataCacheX[num];
+                $scope.comments_result_y = commentsDataCacheY[num];
                 return;
             }
 
@@ -33,39 +33,39 @@ app.controller('dakaqjcvideocomments', ['$rootScope', '$scope', '$http', 'utilsS
                 $http.get("/svc/dakatongji/qjcCommentsCount?num=" + num).success(function (data) {
 
                     var details = data.result.details;
-                    utilsService.formatDataByNo(details, $scope.no, $scope.vote.recentVote);
+                    utilsService.formatDataByNo(details, $scope.no, $scope.CommentsParams.num);
                     var rs = utilsService.getFormatData(details, "no");
                     var ls = utilsService.getFormatData(details, "count");
 
-                    voteDataCacheX[num] = rs;
-                    voteDataCacheY[num] = ls;
+                    commentsDataCacheX[num] = rs;
+                    commentsDataCacheY[num] = ls;
 
-                    $scope.vote_result_x = rs;
-                    $scope.vote_result_y = ls;
+                    $scope.comments_result_x = rs;
+                    $scope.comments_result_y = ls;
 
                 }).error(function (data, status) {
 
-                    console.log("qjcVoteCount in error");
+                    console.log("qjcVideoComments in error");
 
                 });
 
             }).error(function (data, status) {
 
-                console.log("qjcVoteCount in error");
+                console.log("qjcVideoComments in error");
 
             });
         }
 
-        getVote($scope.vote.recentVote);
+        getVote($scope.CommentsParams.num);
 
         //监听投票参数变化
-        $scope.$watch('vote', function (newVal, oldVal) {
+        $scope.$watch('CommentsParams', function (newVal, oldVal) {
 
-            if (newVal !== oldVal && newVal.recentVote !== oldVal.recentVote) {
-                if (!newVal.recentVote || newVal.recentVote < 1 || newVal.recentVote > $scope.no) {
+            if (newVal !== oldVal && newVal.num !== oldVal.num) {
+                if (!newVal.num || newVal.num < 1 || newVal.num > $scope.no) {
                     return;
                 }
-                getVote($scope.vote.recentVote);
+                getVote($scope.CommentsParams.num);
             }
 
         }, true);
