86d03caefd430511ea43d05aa71236db3967c756
puronglong <1240429156@qq.com>
2015-11-02 19:06:41 +0800

    添加URL转码

--- src/statistics-nailstar/static/js/users/userDetails.js
+++ src/statistics-nailstar/static/js/users/userDetails.js
@@ -8,4 +8,15 @@ app.controller('userDetailsCtrl', ['$rootScope', '$scope', '$http', function ($r
         console.log(user.description.id);
     }
 
+    //字符串处理
+    function getQueryString(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r =  decodeURIComponent(window.location.search).substr(1).match(reg);//转码使中文不是乱码
+        if (r != null){
+            return unescape(r[2]);
+        }else{
+            return false;//找不到就返回false
+        }
+    }
+
 }]);
