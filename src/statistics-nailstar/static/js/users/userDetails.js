411607a8aa9e7a3ee97b09b2c1969a4d1457a0de
puronglong <1240429156@qq.com>
2015-11-03 14:47:34 +0800

    搜索框正则修改

--- src/statistics-nailstar/static/js/users/userDetails.js
+++ src/statistics-nailstar/static/js/users/userDetails.js
@@ -1,17 +1,33 @@
 app.controller('userDetailsCtrl', ['$rootScope', '$scope', '$http', function ($rootScope, $scope, $http) {
 
+    var nickname = getQueryString("nickname");
+    if(nickname){
+        $scope.initialValue = nickname;
+    }
+
     $scope.remoteUrlRequestFn = function(str) {
         return {nickname: str};
     };
 
     $scope.userSelected = function(user){
         console.log(user.description.id);
-    }
+
+        $http.get("/svc/dakatongji/findUserDetails" + nickname).success(function (data) {
+
+
+        }).error(function (data, status) {
+
+            console.log("userDetails in error");
+
+        });
+    };
 
     //字符串处理
     function getQueryString(name) {
         var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-        var r =  decodeURIComponent(window.location.search).substr(1).match(reg);//转码使中文不是乱码
+        var uri = decodeURIComponent(window.location.hash);
+        var index = uri.indexOf("?");
+        var r =  uri.substr(index).substr(1).match(reg);//转码使中文不是乱码
         if (r != null){
             return unescape(r[2]);
         }else{
