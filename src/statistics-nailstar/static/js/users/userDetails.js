899632a30a2f9fc7b6e5a5d7dd0f7964b1e6dea0
puronglong <1240429156@qq.com>
2015-11-03 18:59:55 +0800

    修改没有创建时间的情况

--- src/statistics-nailstar/static/js/users/userDetails.js
+++ src/statistics-nailstar/static/js/users/userDetails.js
@@ -17,30 +17,45 @@ app.controller('userDetailsCtrl', ['$rootScope', '$scope', '$http', '$stateParam
         $http.get("/svc/dakatongji/findUserDetails?id=" + user.originalObject.id).success(function (data) {
 
             $scope.infos = data.result;
-            $scope.birthday = moment($scope.infos.birthday).format("YYYY-MM-DD");//出生日期
-            $scope.type = $scope.infos.type;//身份
-            $scope.gender = $scope.infos.gender;//性别
+            $scope.infos.userBirthdaty = moment($scope.infos.birthday).format("YYYY-MM-DD");//出生日期
+            $scope.infos.userType = $scope.infos.type;//身份
+            $scope.infos.userGender = $scope.infos.gender;//性别
+
+            if($scope.infos.birthday){
+                $scope.infos.userBirthdaty = moment($scope.infos.birthday).format("YYYY-MM-DD");//出生日期
+            }else{
+                $scope.infos.birthday = ' ';
+            }
+
+            if(!$scope.infos.creat_date){
+                $scope.infos.creat_date = ' ';
+            }
+
+            var status = ['其他', '美甲老师','美甲消费者', '美甲从业者', '美甲师', '美甲店主', ''];
+            var sex = ['男', '女', '其他'];
 
             if($scope.infos.type === 6){
-                $scope.type = '其他';
+                $scope.infos.userType = status[0];
             }else if($scope.infos.type === 5){
-                $scope.type = '美甲老师';
+                $scope.infos.userType = status[1];
             }else if($scope.infos.type === 4){
-                $scope.type = '美甲消费者';
+                $scope.infos.userType = status[2];
             }else if($scope.infos.type === 3){
-                $scope.type = '美甲从业者';
+                $scope.infos.userType = status[3];
             }else if($scope.infos.type === 2){
-                $scope.type = '美甲师';
+                $scope.infos.userType = status[4];
             }else if($scope.infos.type === 1){
-                $scope.type = '美甲店主';
+                $scope.infos.userType = status[5];
+            }else if($scope.infos.type === 0){
+                $scope.infos.userType = status[6];
             }
 
             if($scope.infos.gender === 2){
-                $scope.gender = '男';
+                $scope.infos.userGender = sex[0];
             }else if($scope.infos.gender === 1){
-                $scope.gender = '女';
+                $scope.infos.userGender = sex[1];
             }else if($scope.infos.gender === 0){
-                $scope.gender = '其他';
+                $scope.infos.userGender = sex[2];
             }
 
         }).error(function (data, status) {
