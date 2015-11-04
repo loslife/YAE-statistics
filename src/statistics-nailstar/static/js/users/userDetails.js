961d282cd0b2d13f3c0c1b535d9a88d33ce6c1c8
keller35 <524495217@qq.com>
2015-11-04 10:51:24 +0800

    评论分页查询

--- src/statistics-nailstar/static/js/users/userDetails.js
+++ src/statistics-nailstar/static/js/users/userDetails.js
@@ -39,63 +39,63 @@ app.controller('userDetailsCtrl', ['$rootScope', '$scope', '$http', '$stateParam
 
         });
 
-	    $scope.filterOptions = {
-		    filterText: "",
-		    useExternalFilter: true
-	    };
-	    $scope.totalServerItems = 0;
-	    $scope.pagingOptions = {
-		    pageSizes: [20, 50, 100],
-		    pageSize: 20,
-		    currentPage: 1
-	    };
-	    $scope.setPagingData = function(data){
-		    console.log(data);
-		    $scope.myData = data;
-		    if (!$scope.$$phase) {
-			    $scope.$apply();
-		    }
-	    };
-	    $scope.getPagedDataAsync = function (pageSize, page) {
-		    $http.get('/svc/dakatongji/findUserCommentDetails?id=' + user.originalObject.id + "&page=" + page + "&perpage=" + pageSize, {}).success(function (data) {
-			    $scope.setPagingData(data.result);
-		    });
-	    };
-	    $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
-
-	    $scope.$watch('pagingOptions', function (newVal, oldVal) {
-		    if (newVal !== oldVal && newVal.currentPage !== oldVal.currentPage) {
-			    $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
-		    }
-	    }, true);
-	    $scope.$watch('filterOptions', function (newVal, oldVal) {
-		    if (newVal !== oldVal) {
-			    $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
-		    }
-	    }, true);
-	    $scope.columnDefs = [
-		    //{field: 'name', displayName: '图片', cellTemplate: '<div style="width: 110px;margin: 0 auto;"><img style="width: 100%;margin-left:10px;" src="{{row.entity.picUrl}}" /></div>'},
-		    {field: 'title', displayName: '标题', cellTemplate: "<span>{{row.entity.title}}</span>"},
-		    {field: 'content', displayName: '内容', cellTemplate: "<span>{{row.entity.content}}</span>"},
-		    {field: 'create_date', displayName: '创建时间', cellTemplate: '<span>{{row.entity.create_date}}</span>'},
-	    ];
-
-        $scope.myData = [];
-	    $scope.gridOptions = {
-		    data: 'myData',
-		    columnDefs: $scope.columnDefs,
-		    enablePaging: true,
-		    showFooter: true,
-		    rowHeight:60,
-		    pagingOptions: $scope.pagingOptions,
-		    filterOptions: $scope.filterOptions,
-		    multiSelect: false,
-		    i18n: 'zh_cn'
-	    };
-	    window.ngGrid.i18n['zh_cn'] = yilos_i18n.resource;
+		$scope.getPagedDataAsync(user.originalObject.id, $scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
 
     };
 
 
+	$scope.filterOptions = {
+		filterText: "",
+		useExternalFilter: true
+	};
+	$scope.totalServerItems = 0;
+	$scope.pagingOptions = {
+		pageSizes: [20, 50, 100],
+		pageSize: 20,
+		currentPage: 1
+	};
+	$scope.setPagingData = function(data){
+		console.log(data);
+		$scope.myData = data;
+		if (!$scope.$$phase) {
+			$scope.$apply();
+		}
+	};
+	$scope.getPagedDataAsync = function (id, pageSize, page) {
+		$http.get('/svc/dakatongji/findUserCommentDetails?id=' + id + "&page=" + page + "&perpage=" + pageSize, {}).success(function (data) {
+			$scope.setPagingData(data.result);
+		});
+	};
+
+	$scope.$watch('pagingOptions', function (newVal, oldVal) {
+		if (newVal !== oldVal && newVal.currentPage !== oldVal.currentPage) {
+			$scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
+		}
+	}, true);
+	$scope.$watch('filterOptions', function (newVal, oldVal) {
+		if (newVal !== oldVal) {
+			$scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
+		}
+	}, true);
+	$scope.columnDefs = [
+		//{field: 'name', displayName: '图片', cellTemplate: '<div style="width: 110px;margin: 0 auto;"><img style="width: 100%;margin-left:10px;" src="{{row.entity.picUrl}}" /></div>'},
+		{field: 'title', displayName: '标题', cellTemplate: "<span>{{row.entity.title}}</span>"},
+		{field: 'content', displayName: '内容', cellTemplate: "<span>{{row.entity.content}}</span>"},
+		{field: 'create_date', displayName: '创建时间', cellTemplate: '<span>{{row.entity.create_date}}</span>'},
+	];
+
+	$scope.myData = [];
+	$scope.gridOptions = {
+		data: 'myData',
+		columnDefs: $scope.columnDefs,
+		enablePaging: true,
+		showFooter: true,
+		rowHeight:60,
+		pagingOptions: $scope.pagingOptions,
+		filterOptions: $scope.filterOptions,
+		multiSelect: false,
+		i18n: 'zh_cn'
+	};
+	window.ngGrid.i18n['zh_cn'] = yilos_i18n.resource;
 
 }]);
